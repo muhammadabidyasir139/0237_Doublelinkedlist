d266f0785ed33af10a0503200ce32c3a26d7641d
wesclicdev <wesclicdev@gmail.com>
2024-05-03 22:50:09 +0700

    membuat new node berpindah ke node selanjutnya

--- singlelink2.cpp
+++ singlelink2.cpp
@@ -31,4 +31,9 @@ void addNode() {
 } else {
     Node *current = START;
     Node *previous = NULL;
+
+    while (current != NULL && current->noMhs < newNode->noMhs) {
+        previous = current;
+        current = current->next;
+    }
 }
