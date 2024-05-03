a18a514a30af465b528b5ea9db88e24d2609cb62
wesclicdev <wesclicdev@gmail.com>
2024-05-03 22:36:32 +0700

    membuat new node

--- singlelink2.cpp
+++ singlelink2.cpp
@@ -1,2 +1,21 @@
 #include <iostream>;
 using namespace std;
+
+struct Node {
+    int noMhs;
+    string name;
+    Node *next;
+    Node *prev;
+};
+
+Node *START = NULL;
+
+void addNode() {
+    Node *newNode = new Node();
+    cout << "/nEnter the roll number of the student: ";
+    cin >> newNode->noMhs;
+    cout << "/nEnter the name of the student: ";
+    cin >> newNode->name;
+
+    
+}
