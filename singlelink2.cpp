7ff13e3a3a7c846fcd1a1ecc328d31cf6effbdc5
wesclicdev <wesclicdev@gmail.com>
2024-05-03 22:39:52 +0700

    membuat memasukan node baru ke list

--- singlelink2.cpp
+++ singlelink2.cpp
@@ -17,5 +17,8 @@ void addNode() {
     cout << "/nEnter the name of the student: ";
     cin >> newNode->name;
 
-    
+    if(START != NULL && newNode->noMhs == START->noMhs) {
+        cout << "\033[31mDuplicate roll numbers not allowed\033[0m" << endl;
+        return;
+    }
 }
