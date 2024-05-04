a0951c84f95c6f67e411cd01e59ebc2b0adc07a8
wesclicdev <wesclicdev@gmail.com>
2024-05-04 09:48:29 +0700

    membuat tampilan jika data telah di hapus

--- singlelink2.cpp
+++ singlelink2.cpp
@@ -106,5 +106,7 @@ void deleteNode()
             current->next->prev = previous;
         }
     }
+    delete current;
+    cout << "\x1b[32mRecord with roll number" << rollNo << "deleted\x1b[0m" << endl; 
 
 }
