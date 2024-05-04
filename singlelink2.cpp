2f5f29caad2bff42d912a19784f392546fec3e4e
wesclicdev <wesclicdev@gmail.com>
2024-05-04 10:00:05 +0700

    Tambahkan fungsi revtraverse untuk mencetak catatan dalam urutan nomor roll menurun

--- singlelink2.cpp
+++ singlelink2.cpp
@@ -126,7 +126,26 @@ void travrese()
         while (currentNode != NULL)
         {
             cout << currentNode->noMhs << "" << currentNode->name <<endl;
-            currentNode = currentNode-next;
+            currentNode = currentNode->next;
+        }
+    }
+}
+
+void revtraverse() 
+{
+    if (listEmpty())
+        cout << "\nList is empty" << endl;
+    else
+    {
+        cout << "\nRecords in descending order of roll number are:" << endl;
+        Node *currentNode = START;
+        while (currentNode->next != NULL)
+            currentNode = currentNode->next;
+
+        while(currentNode != NULL)
+        {
+            cout << currentNode->noMhs << "" << currentNode->name <<endl;
+            currentNode = currentNode->prev;
         }
     }
 }
